dbe7439a5914e9645fe61d54263f132ef12c2120
Sergio Neves <s.neve20@gmail.com>
2021-09-19 10:42:43 +0100

    Adding methods to work with brokers

--- API/utils/brokers/degiro.py
+++ API/utils/brokers/degiro.py
@@ -1,20 +1,28 @@
 from brokers import Broker
 import degiroapi
 
-
 class DegiroBroker(Broker):
+
     def __init__(self, username, password):
         super().__init__(username, password)
         self.degiro = degiroapi.DeGiro()
-
+    
     def login(self):
 
         self.degiro.login(self.username, self.password)
 
+
     def logout(self):
 
         self.degiro.logout()
+    
 
-    def get_portfolio(self):
+    def get_portfolio(self) -> list:
 
-        portfolio = degiro.getdata(degiroapi.Data.Type.PORTFOLIO, True)
+        return degiro.getdata(degiroapi.Data.Type.PORTFOLIO, True)
+
+    @static_method
+    def filter_portfolio(item: dict):
+        
+        
+        
